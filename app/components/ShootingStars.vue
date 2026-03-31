d3be7f2c7dc4ef72cd1b620ae568d5c22929d3a6
propanto <prpanto@gmail.com>
2026-03-31 15:29:49 +0300

    Add login page design and logic

---
+++ app/components/ShootingStars.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    count?: number
+    starsColor?: string
+  }>(),
+  {
+    count: 200,
+    starsColor: '#fff'
+  }
+);
+</script>
+
+<template>
+  <div class="fixed size-full flex justify-center items-center">
+    <div class="absolute inset-0 size-full overflow-hidden perspective-[10vmin]">
+      <ClientOnly>
+        <div v-for="star in count" :key="'star_' + star"
+          class="animate-stars size-[1.5vmin] absolute shadow-[0_0_20px_rgb(10,239,255)] transform-[translateZ(-100vmin)_rotateY(90deg)_rotateX(var(--rx))_translateZ(var(--x))_scaleX(1)]"
+          :style="{
+            backgroundColor: starsColor,
+            '--x': `${Math.random() * 200}vmax`,
+            '--y': `${Math.random() * 100}vh`,
+            '--z': `${Math.random() * 200 - 100}vmin`,
+            '--rx': `${Math.random() * 360}deg`,
+            animationDelay: `${Math.random() * 1.5}s`,
+          }" />
+      </ClientOnly>
+    </div>
+
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.animate-stars {
+  animation: stars 1.5s infinite ease-in;
+}
+
+@keyframes stars {
+
+  0%,
+  90% {
+    opacity: 1;
+  }
+
+  100% {
+    transform: translateZ(0vmin) rotateY(90deg) rotateX(var(--rx)) translateZ(var(--x)) scaleX(6);
+  }
+}
+</style>
--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,7 +1,20 @@
-<script lang="ts" setup>
-
+<script setup lang="ts">
+const login = async () => {
+  await auth.signIn.social({
+    provider: "github",
+  });
+}
 </script>
 
 <template>
-  <h1>Login</h1>
+  <ShootingStars class="bg-gray-950">
+    <div class="flex flex-col items-center justify-center gap-4 z-10">
+      <div class="flex items-center gap-4">
+        <Logo />
+        <h2 class="text-white text-5xl font-bold font-nasa tracking-wider uppercase">Astrox</h2>
+      </div>
+
+      <UButton icon="i-lucide-log-in" size="xl" color="neutral" variant="solid" @click="login">Login</UButton>
+    </div>
+  </ShootingStars>
 </template>
