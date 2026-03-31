a38968b504d0846c12f891ca6b8e91118780d4e0
propanto <prpanto@gmail.com>
2026-03-31 13:14:19 +0300

    Install nuxt ui

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <NuxtPage />
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  compatibilityDate: "2025-07-15",
+
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxt/ui",
+  ],
+
+  css: [
+    "~/assets/css/main.css",
+  ],
 })
